8ba7a05c174cb6e5ac274833b03e67334e071038
iamsalil <salil512@stanford.edu>
2024-11-10 16:25:04 -0600

    Update cudaRenderer.cu

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -723,16 +723,16 @@ void multiExclusiveScan_MultiBlock(int* deviceArr, int width, int height, int le
     for (int i = 0; i < numBlocksPerTile; i++) {
         printf("block %d: ", i);
         kernelPrintArrV2<<<1, 1>>>(deviceArr, 2080*length+256*i, 256);
+        cudaDeviceSynchronize();
     }
-    cudaDeviceSynchronize();
     kernelMultiExclusiveScan_MultiBlock<<<gridDim, blockDim>>>(deviceArr, length);
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
     for (int i = 0; i < numBlocksPerTile; i++) {
         printf("block %d: ", i);
         kernelPrintArrV2<<<1, 1>>>(deviceArr, 2080*length+256*i, 256);
+        cudaDeviceSynchronize();
     }
-    cudaDeviceSynchronize();
     if (numBlocksPerTile <= 32) {
         // Part 2 (32) - Add blocks together
         printf("    > part 2\n");
